41e067c024f4e70fa8eeccd5f2a8798b7a84ead1
nazibur-175 <nazibur175@gmail.com>
2023-02-23 23:08:42 +0600

    function for trinagle Rhombus and pentagon added

---
+++ js/index-js.js
@@ -0,0 +1,8 @@
+function areaCalculatorOfTriangleOrRhombusOrPentagon(firstId,secondId){
+  const x = parseFloat(document.getElementById(firstId).value);
+  document.getElementById(firstId).value='';
+  const y = parseFloat(document.getElementById(secondId).value);
+  document.getElementById(secondId).value='';
+  const A=0.5*x*y;
+  return A;
+}
