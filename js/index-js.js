46de9ff0ff456da78bf33642f6afee19787974eb
nazibur-175 <nazibur175@gmail.com>
2023-02-23 23:11:09 +0600

    function for rectangle and parallelogram added

--- js/index-js.js
+++ js/index-js.js
@@ -6,3 +6,12 @@ function areaCalculatorOfTriangleOrRhombusOrPentagon(firstId,secondId){
   const A=0.5*x*y;
   return A;
 }
+
+function areaCalculatorOfRectangleOrParallelogram(firstId,secondId){
+  const x = parseFloat(document.getElementById(firstId).value);
+  document.getElementById(firstId).value='';
+  const y = parseFloat(document.getElementById(secondId).value);
+  document.getElementById(secondId).value='';
+  const A=x*y;
+  return A;
+}
